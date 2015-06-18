f29f7da9ee0b793284152dd1605bb31ab7b44469
ghm231988 <ghm231988@gmail.com>
2015-06-18 16:17:24 +0530

    hellomaven repo

---
+++ src/main/hellomaven/HelloMavenMain.java
@@ -0,0 +1,11 @@
+package com.example.hellomaven;
+
+public class HelloMavenMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger logger = LoggerFactory.getLogger(HelloMavenMain.class);
+		logger.info("Hello Maven!");
+	}
+
+}
